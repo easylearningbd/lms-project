9cc19a3cce23f9f34b21e69abf2e1986d70e8709
easylearningbd <elebdsoft@gmail.com>
2023-08-18 02:53:50 +0600

    16 Admin Profile & Image Update Part 2

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -10,7 +10,7 @@
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">User Profilep</li>
+                    <li class="breadcrumb-item active" aria-current="page">User Profile</li>
                 </ol>
             </nav>
         </div>
@@ -26,14 +26,14 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center text-center">
-                                <img src="assets/images/avatars/avatar-2.png" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
-                                <div class="mt-3">
-                                    <h4>John Doe</h4>
-                                    <p class="text-secondary mb-1">Full Stack Developer</p>
-                                    <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                                    <button class="btn btn-primary">Follow</button>
-                                    <button class="btn btn-outline-primary">Message</button>
-                                </div>
+    <img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+    <div class="mt-3">
+        <h4>{{ $profileData->name }}</h4>
+        <p class="text-secondary mb-1">{{ $profileData->username }}</p>
+        <p class="text-muted font-size-sm">{{ $profileData->email }}</p>
+        <button class="btn btn-primary">Follow</button>
+        <button class="btn btn-outline-primary">Message</button>
+    </div>
                             </div>
                             <hr class="my-4" />
                             <ul class="list-group list-group-flush">
@@ -46,59 +46,80 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-8">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Full Name</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="John Doe" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Email</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="john@example.com" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Phone</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="(239) 816-9029" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Mobile</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="(320) 380-4539" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Address</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="Bay Area, San Francisco, CA" />
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-3"></div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="button" class="btn btn-primary px-4" value="Save Changes" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
+<div class="col-lg-8">
+    <div class="card">
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Name</h6>
                 </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="text" name="name" class="form-control" value="{{ $profileData->name }}" />
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">User Name</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="text" name="username" class="form-control" value="{{ $profileData->username }}" />
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Email</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="email" name="email" class="form-control" value="{{ $profileData->email }}" />
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Phone</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}" />
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Address</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="text" name="address" class="form-control" value="{{ $profileData->address }}" />
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Profile Image </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="file" name="photo" class="form-control"   />
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">  </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
+                </div>
+            </div>
+
+
+
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-9 text-secondary">
+                    <input type="button" class="btn btn-primary px-4" value="Save Changes" />
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
             </div>
         </div>
     </div>
