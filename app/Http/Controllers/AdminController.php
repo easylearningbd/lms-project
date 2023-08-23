20daf469d44a9a0c0a6b87081359a1c598dc7632
easylearningbd <elebdsoft@gmail.com>
2023-08-24 02:06:33 +0600

    54  Active and Inactive Instructor Part 3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -145,6 +145,21 @@ class AdminController extends Controller
         return view('admin.backend.instructor.all_instructor',compact('allinstructor'));
     }// End Method
 
+    public function UpdateUserStatus(Request $request){
+
+        $userId = $request->input('user_id');
+        $isChecked = $request->input('is_checked',0);
+
+        $user = User::find($userId);
+        if ($user) {
+            $user->status = $isChecked;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'User Status Updated Successfully']);
+
+    }// End Method
+
 
 }
  
