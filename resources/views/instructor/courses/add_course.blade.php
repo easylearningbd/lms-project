177af984e3917cb2ebe4d85fe204b39e22dac93a
easylearningbd <elebdsoft@gmail.com>
2023-08-25 02:39:06 +0600

    61 Add Goal Option In Add Course Page

--- resources/views/instructor/courses/add_course.blade.php
+++ resources/views/instructor/courses/add_course.blade.php
@@ -130,6 +130,27 @@
                 <textarea name="description" class="form-control" id="myeditorinstance"></textarea>
             </div>
 
+
+            <p>Course Goals </p>
+             
+<!--   //////////// Goal Option /////////////// -->
+
+        <div class="row add_item">
+        
+            <div class="col-md-6">
+                  <div class="mb-3">
+                        <label for="goals" class="form-label"> Goals </label>
+                        <input type="text" name="course_goals[]" id="goals" class="form-control" placeholder="Goals ">
+                  </div>
+            </div>
+            <div class="form-group col-md-6" style="padding-top: 30px;">
+                  <a class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i> Add More..</a>
+            </div>
+     </div> <!---end row-->
+
+<!--   //////////// End Goal Option /////////////// -->
+
+
             <hr>
             <div class="row">
 
@@ -170,6 +191,48 @@
  
    
 </div>
+
+
+<!--========== Start of add multiple class with ajax ==============-->
+<div style="visibility: hidden">
+    <div class="whole_extra_item_add" id="whole_extra_item_add">
+       <div class="whole_extra_item_delete" id="whole_extra_item_delete">
+          <div class="container mt-2">
+             <div class="row">
+               
+                
+                <div class="form-group col-md-6">
+                   <label for="goals">Goals</label>
+                   <input type="text" name="course_goals[]" id="goals" class="form-control" placeholder="Goals  ">
+                </div>
+                <div class="form-group col-md-6" style="padding-top: 20px">
+                   <span class="btn btn-success btn-sm addeventmore"><i class="fa fa-plus-circle">Add</i></span>
+                   <span class="btn btn-danger btn-sm removeeventmore"><i class="fa fa-minus-circle">Remove</i></span>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>      
+ 
+  
+ <!----For Section-------->
+ <script type="text/javascript">
+    $(document).ready(function(){
+       var counter = 0;
+       $(document).on("click",".addeventmore",function(){
+             var whole_extra_item_add = $("#whole_extra_item_add").html();
+             $(this).closest(".add_item").append(whole_extra_item_add);
+             counter++;
+       });
+       $(document).on("click",".removeeventmore",function(event){
+             $(this).closest("#whole_extra_item_delete").remove();
+             counter -= 1
+       });
+    });
+ </script>
+ <!--========== End of add multiple class with ajax ==============-->
+
 
 
 <script type="text/javascript">
