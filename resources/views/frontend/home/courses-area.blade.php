f149ba288c5c3d603c6163759739f5a8a52c6f76
easylearningbd <elebdsoft@gmail.com>
2023-09-03 03:12:07 +0600

    90 Home Featured Course Display Part 4

--- resources/views/frontend/home/courses-area.blade.php
+++ resources/views/frontend/home/courses-area.blade.php
@@ -35,7 +35,7 @@
       @foreach ($courses as $course) 
                      
     <div class="col-lg-4 responsive-column-half">
-        <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1">
+        <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1{{ $course->id }}">
             <div class="card-image">
                 <a href="course-details.html" class="d-block">
                     <img class="card-img-top lazy" src="{{ asset($course->course_image) }}" data-src="images/img8.jpg" alt="Card image cap">
@@ -170,3 +170,51 @@
         </div><!-- end container -->
     </div><!-- end card-content-wrapper -->
 </section><!-- end courses-area -->
+
+
+@php
+    $courseData = App\Models\Course::get();
+@endphp
+
+<!-- tooltip_templates -->
+@foreach ($courseData as $item)
+     
+<div class="tooltip_templates">
+    <div id="tooltip_content_1{{ $item->id }}">
+        <div class="card card-item">
+            <div class="card-body">
+                <p class="card-text pb-2">By <a href="teacher-detail.html">{{ $item['user']['name'] }}</a></p>
+                <h5 class="card-title pb-1"><a href="course-details.html"> {{ $item->course_name }}</a></h5>
+                <div class="d-flex align-items-center pb-1">
+                    @if ($item->bestseller == 1)
+                    <h6 class="ribbon fs-14 mr-2">Bestseller</h6>
+                    @else
+                    <h6 class="ribbon fs-14 mr-2">New</h6> 
+                    @endif
+                   
+                    <p class="text-success fs-14 font-weight-medium">Updated<span class="font-weight-bold pl-1">{{ $item->created_at->format('M d Y') }}</span></p>
+                </div>
+                <ul class="generic-list-item generic-list-item-bullet generic-list-item--bullet d-flex align-items-center fs-14">
+                    <li>{{ $item->duration }} total hours</li>
+                    <li>{{ $item->label }}</li>
+                </ul>
+                <p class="card-text pt-1 fs-14 lh-22">{{ $item->prerequisites }}</p>
+
+    @php
+       $goals = App\Models\Course_goal::where('course_id',$item->id)->orderBy('id','DESC')->get(); 
+    @endphp
+                <ul class="generic-list-item fs-14 py-3">
+                    @foreach ($goals as $goal)
+                    <li><i class="la la-check mr-1 text-black"></i> {{ $goal->goal_name }}</li> 
+                    @endforeach
+                </ul>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#" class="btn theme-btn flex-grow-1 mr-3"><i class="la la-shopping-cart mr-1 fs-18"></i> Add to Cart</a>
+                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
+                </div>
+            </div>
+        </div><!-- end card -->
+    </div>
+</div><!-- end tooltip_templates -->
+@endforeach
+ 
