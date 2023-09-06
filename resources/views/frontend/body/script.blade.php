8614fadbfc0c66ad5b5964dcd6e3487cc5685784
easylearningbd <elebdsoft@gmail.com>
2023-09-07 03:22:57 +0600

    113 Wishlist Read Course With Ajax Part 2

--- resources/views/frontend/body/script.blade.php
+++ resources/views/frontend/body/script.blade.php
@@ -62,10 +62,44 @@
             url: "/get-wishlist-course/",
 
             success:function(response){
-                
+
+                var rows = ""
+                $.each(response.wishlist, function(key, value){
+
+            rows += `
+                    <div class="col-lg-4 responsive-column-half">
+            <div class="card card-item">
+                <div class="card-image">
+                    <a href="course-details.html" class="d-block">
+                        <img class="card-img-top" src="/${value.course.course_image}" alt="Card image cap">
+                    </a>
+                  
+                </div><!-- end card-image -->
+                <div class="card-body">
+                    <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">${value.course.label}</h6>
+                    <h5 class="card-title"><a href="course-details.html">${value.course.course_name}</a></h5> 
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        
+                        ${value.course.discount_price == null 
+                        ?`<p class="card-price text-black font-weight-bold">$${value.course.selling_price}</p>`
+                        :`<p class="card-price text-black font-weight-bold">$${value.course.discount_price} <span class="before-price font-weight-medium">$${value.course.selling_price}</span></p>`
+                        } 
+                       
+                        <div class="icon-element icon-element-sm shadow-sm cursor-pointer" data-toggle="tooltip" data-placement="top" title="Remove from Wishlist"><i class="la la-heart"></i></div>
+                    </div>
+                </div> 
+            </div> 
+        </div> 
+             `
+                });
+               $('#wishlist').html(rows); 
+
             }
         })
     }
+    wishlist();
+
 
  </script>
   {{-- /// End Load Wishlist Data // --}}
--- resources/views/frontend/wishlist/all_wishlist.blade.php
+++ resources/views/frontend/wishlist/all_wishlist.blade.php
@@ -7,42 +7,10 @@
     <div class="dashboard-heading mb-5">
         <h3 class="fs-22 font-weight-semi-bold">My Bookmarks</h3>
     </div>
-    <div class="row">
+    <div class="row" id="wishlist">
        
        
-        <div class="col-lg-4 responsive-column-half">
-            <div class="card card-item">
-                <div class="card-image">
-                    <a href="course-details.html" class="d-block">
-                        <img class="card-img-top" src="images/img8.jpg" alt="Card image cap">
-                    </a>
-                    <div class="course-badge-labels">
-                        <div class="course-badge">Bestseller</div>
-                        <div class="course-badge blue">-39%</div>
-                    </div>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                    <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">All Levels</h6>
-                    <h5 class="card-title"><a href="course-details.html">The Business Intelligence Analyst Course 2021</a></h5>
-                    <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                    <div class="rating-wrap d-flex align-items-center py-2">
-                        <div class="review-stars">
-                            <span class="rating-number">4.4</span>
-                            <span class="la la-star"></span>
-                            <span class="la la-star"></span>
-                            <span class="la la-star"></span>
-                            <span class="la la-star"></span>
-                            <span class="la la-star-o"></span>
-                        </div>
-                        <span class="rating-total pl-1">(20,230)</span>
-                    </div><!-- end rating-wrap -->
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p class="card-price text-black font-weight-bold">12.99 <span class="before-price font-weight-medium">129.99</span></p>
-                        <div class="icon-element icon-element-sm shadow-sm cursor-pointer" data-toggle="tooltip" data-placement="top" title="Remove from Wishlist"><i class="la la-heart"></i></div>
-                    </div>
-                </div><!-- end card-body -->
-            </div><!-- end card -->
-        </div><!-- end col-lg-4 -->
+       
 
 
         
