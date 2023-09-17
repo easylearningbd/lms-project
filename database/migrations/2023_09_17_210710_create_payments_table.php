5d58f590bda901552302ba336fb84ad139cf25a2
easylearningbd <elebdsoft@gmail.com>
2023-09-18 03:14:49 +0600

    145 Add Cash on Delivery Payment Method  Part 1

---
+++ database/migrations/2023_09_17_210710_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('cash_delivery')->nullable();
+            $table->string('total_amount')->nullable();
+            $table->string('payment_type')->nullable();
+            $table->string('invoice_no')->nullable();
+            $table->string('order_date')->nullable();
+            $table->string('order_month')->nullable();
+            $table->string('order_year')->nullable();
+            $table->string('status')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
---
+++ database/migrations/2023_09_17_210737_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('payment_id');
+            $table->integer('user_id')->nullable();
+            $table->integer('course_id')->nullable();
+            $table->integer('instructor_id')->nullable();
+            $table->string('image')->nullable();
+            $table->string('course_title')->nullable();
+            $table->integer('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
