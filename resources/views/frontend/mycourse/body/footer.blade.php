9238d79215b1cde7734bf28cd624dabe2673abf6
easylearningbd <elebdsoft@gmail.com>
2023-09-21 01:33:21 +0600

    169 User Course Enrollment Setup Part 2

---
+++ resources/views/frontend/mycourse/body/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- template js files -->
+<script src="{{ asset('frontend/js/jquery-3.4.1.min.js') }}"></script>
+<script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('frontend/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('frontend/js/isotope.js') }}"></script>
+<script src="{{ asset('frontend/js/waypoint.min.js') }}"></script>
+<script src="{{ asset('frontend/js/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('frontend/js/fancybox.js') }}"></script>
+<script src="{{ asset('frontend/js/plyr.js') }}"></script>
+<script src="{{ asset('frontend/js/datedropper.min.js') }}"></script>
+<script src="{{ asset('frontend/js/emojionearea.min.js') }}"></script>
+<script src="{{ asset('frontend/js/jquery-te-1.4.0.min.js') }}"></script>
+<script src="{{ asset('frontend/js/jquery.MultiFile.min.js') }}"></script>
+<script src="{{ asset('frontend/js/main.js') }}"></script>
+<script>
+    var player = new Plyr('#player');
+</script>
---
+++ resources/views/frontend/mycourse/body/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="author" content="TechyDevs">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Course Details </title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" sizes="16x16" href=" {{ asset('frontend/images/favicon.png') }}">
+  
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/line-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/fancybox.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/animated-headline.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/plyr.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery-te-1.4.0.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    <!-- end inject -->
+</head>
