5c91f1018fbb44372f5165f19f24dccbd0fd474d
easylearningbd <elebdsoft@gmail.com>
2023-10-02 02:13:40 +0600

    204 Course Review System Part 5

--- resources/views/frontend/course/course_details.blade.php
+++ resources/views/frontend/course/course_details.blade.php
@@ -24,18 +24,62 @@
                     <h6 class="ribbon ribbon-lg mr-2 bg-3 text-white">Bestseller</h6>
                     @else
                     @endif 
+
+   @php
+       $reviewcount = App\Models\Review::where('course_id',$course->id)->where('status',1)->latest()->get();
+       $avarage = App\Models\Review::where('course_id',$course->id)->where('status',1)->avg('rating');
+
+   @endphp                 
                     
                     <div class="rating-wrap d-flex flex-wrap align-items-center">
                         <div class="review-stars">
-                            <span class="rating-number">4.4</span>
+                            <span class="rating-number">{{ round($avarage,1) }}</span>
+                        
+                            @if ($avarage == 0)
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            @elseif ($avarage == 1 || $avarage < 2)
+                            <span class="la la-star"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            @elseif ($avarage == 2 || $avarage < 3)
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            @elseif ($avarage == 3 || $avarage < 4)
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star-o"></span>
+                            <span class="la la-star-o"></span>
+                            @elseif ($avarage == 4 || $avarage < 5)
                             <span class="la la-star"></span>
                             <span class="la la-star"></span>
                             <span class="la la-star"></span>
                             <span class="la la-star"></span>
                             <span class="la la-star-o"></span>
+                            @elseif ($avarage == 5 || $avarage < 5)
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            <span class="la la-star"></span>
+                            @endif 
+                            
                         </div>
-                        <span class="rating-total pl-1">(20,230 ratings)</span>
-                        <span class="student-total pl-2">540,815 students</span>
+                        <span class="rating-total pl-1">({{ count($reviewcount) }} ratings)</span>
+    @php
+        $enrollmentCount = App\Models\Order::where('course_id',$course->id)->count();
+    @endphp
+
+                        <span class="student-total pl-2">{{ number_format($enrollmentCount) }} students</span>
                     </div>
                 </div><!-- end d-flex -->
                 <p class="pt-2 pb-1">Created by <a href="teacher-detail.html" class="text-color hover-underline">{{ $course['user']['name'] }}</a></p>
