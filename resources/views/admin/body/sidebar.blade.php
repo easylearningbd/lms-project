bb1542cfe95980a2a511e23ecb174710a2777a10
easylearningbd <elebdsoft@gmail.com>
2023-10-07 02:32:42 +0600

    237 Setup User Permission Part 1

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -186,20 +186,17 @@
          
       
      
-        <li class="menu-label">Charts & Maps</li>
+        <li class="menu-label">Role & Permission</li>
         <li>
             <a class="has-arrow" href="javascript:;">
                 <div class="parent-icon"><i class="bx bx-line-chart"></i>
                 </div>
-                <div class="menu-title">Charts</div>
+                <div class="menu-title">Role & Permission</div>
             </a>
             <ul>
-                <li> <a href="charts-apex-chart.html"><i class='bx bx-radio-circle'></i>Apex</a>
+                <li> <a href="{{ route('all.permission') }}"><i class='bx bx-radio-circle'></i>All Permission</a>
                 </li>
-                <li> <a href="charts-chartjs.html"><i class='bx bx-radio-circle'></i>Chartjs</a>
-                </li>
-                <li> <a href="charts-highcharts.html"><i class='bx bx-radio-circle'></i>Highcharts</a>
-                </li>
+                
             </ul>
         </li>
         <li>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\Backend\ReportController;
 use App\Http\Controllers\Backend\ReviewController;
 use App\Http\Controllers\Backend\ActiveUserController;
 use App\Http\Controllers\Backend\BlogController;
+use App\Http\Controllers\Backend\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -226,6 +227,15 @@ Route::controller(BlogController::class)->group(function(){
     Route::get('/edit/post/{id}','EditBlogPost')->name('edit.post');  
     Route::post('/update/blog/post','UpdateBlogPost')->name('update.blog.post');
     Route::get('/delete/post/{id}','DeleteBlogPost')->name('delete.post');  
+  
+    
+});
+
+
+// Permission All Route 
+Route::controller(RoleController::class)->group(function(){
+    Route::get('/all/permission','AllPermission')->name('all.permission'); 
+    
   
     
 });
